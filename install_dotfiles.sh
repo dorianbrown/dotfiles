fbdaac918e584260fa0b3a6fe953f86054febd9d
Dorian Brown <dorianstuartbrown@gmail.com>
2017-06-12 20:31:12 +0200

    Small changes

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Create repos directory
-mkdir ~/repos
-cd ~/repos
-git clone
-
-
 # Cleanup for testing
 rm -rf ~/repos
+
+# Install Chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+rm google-chrome*.deb
+
+# Install Steam
+wget -E http://media.steampowered.com/client/installer/steam.deb
+sudo dpkg -i steam.deb
