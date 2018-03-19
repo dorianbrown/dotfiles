ccf6eb7fb22d450ff1d3c5fa349022d42a5f03e8
Dorian Brown <dorianstuartbrown@gmail.com>
2018-03-19 18:39:52 +0100

    Removed cloning of data repo

--- install_scripts/setup_gitrepos.sh
+++ install_scripts/setup_gitrepos.sh
@@ -7,6 +7,5 @@ git config --global user.email dorianstuartbrown@gmail.com
 # Cloning repositories
 cd ~/workspace
 git clone https://github.com/dorianbrown/dorianbrown.github.io 
-git clone https://github.com/fivethirtyeight/data data_538
 
 cd -
